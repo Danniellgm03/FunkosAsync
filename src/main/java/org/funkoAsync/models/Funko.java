34ffb3aa7b958fbae3244d8c6d2adbf93f14fa66
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-10-04 09:50:43 +0200

    feat(locale): Creamos locale y lo utilizamos en funko

--- src/main/java/org/funkoAsync/models/Funko.java
+++ src/main/java/org/funkoAsync/models/Funko.java
@@ -1,11 +1,24 @@
 package org.funkoAsync.models;
 
 import org.funkoAsync.enums.Modelo;
+import org.funkoAsync.locale.MyLocale;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record Funko(Integer id, UUID COD, String nombre, Modelo modelo, double precio, LocalDate fecha, LocalDateTime created_at, LocalDateTime updated_at) {
-    
+    @Override
+    public String toString() {
+        return "Funko{" +
+                "id=" + id +
+                ", COD=" + COD +
+                ", nombre='" + nombre + '\'' +
+                ", modelo=" + modelo +
+                ", precio=" + MyLocale.toLocalMoney(precio) +
+                ", fecha=" + MyLocale.toLocalDate(fecha) +
+                ", created_at=" + MyLocale.toLocalDateTime(created_at) +
+                ", updated_at=" + MyLocale.toLocalDateTime(updated_at) +
+                '}';
+    }
 }
