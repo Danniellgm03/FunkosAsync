a38f070b6a1afe952d78c11625fb3251dfff4f60
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-10-04 09:47:15 +0200

    ci(dependencies): Quitamos lombok y projectreactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("ch.qos.logback:logback-classic:1.4.11")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
     implementation("org.jetbrains:annotations:24.0.0")
     implementation("com.h2database:h2:2.2.224")
     implementation("org.mybatis:mybatis:3.5.13")
-    implementation("io.projectreactor:reactor-core:3.5.10")
 }
 
 tasks.test {
