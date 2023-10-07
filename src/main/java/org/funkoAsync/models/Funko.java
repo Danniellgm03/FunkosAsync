cb35e015ce41df6ee1fd3330b648ff1da17d2a2e
daniel <daniell.gm03@gmail.com>
2023-10-07 12:08:45 +0200

    fix(models): Cambiamos de record a class para poder asignarle id de la bd

--- src/main/java/org/funkoAsync/models/Funko.java
+++ src/main/java/org/funkoAsync/models/Funko.java
@@ -7,18 +7,85 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Funko(UUID COD, Long myId, String nombre, Modelo modelo, double precio, LocalDate fecha, LocalDateTime created_at, LocalDateTime updated_at) {
+public class Funko {
+    private Integer id;
+    private UUID COD;
+    private Long myId;
+    private String nombre;
+    private Modelo modelo;
+    private double precio;
+    private LocalDate fecha;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public Funko(Integer id, UUID COD, Long myId, String nombre, Modelo modelo, double precio, LocalDate fecha, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.id = id;
+        this.COD = COD;
+        this.myId = myId;
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.fecha = fecha;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public UUID getCOD() {
+        return COD;
+    }
+
+    public Long getMyId() {
+        return myId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Funko{" +
-                "COD=" + COD +
+                "id=" + id +
+                ", COD=" + COD +
                 ", myId=" + myId +
                 ", nombre='" + nombre + '\'' +
                 ", modelo=" + modelo +
-                ", precio=" + precio +
-                ", fecha=" + fecha +
-                ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
+                ", precio=" + MyLocale.toLocalMoney(precio) +
+                ", fecha=" + MyLocale.toLocalDate(fecha) +
+                ", created_at=" + MyLocale.toLocalDateTime(created_at) +
+                ", updated_at=" + MyLocale.toLocalDateTime(updated_at) +
                 '}';
     }
 }
